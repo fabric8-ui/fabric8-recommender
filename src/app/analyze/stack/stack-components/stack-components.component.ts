e7ea796ab63f04e3a5a789a2ab3a95ca902255e6
jaivardhan <mailjai.vardhan@gmail.com>
2017-02-23 13:58:52 +0530

    created template for dependency grid with patternfly, added to modal

--- src/app/analyze/stack/stack-components/stack-components.component.ts
+++ src/app/analyze/stack/stack-components/stack-components.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'f8-stack-components',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class StackComponents {
 
+    @Input() dependencies;
+    private dependenciesList: Array<any> = [];
+
+    constructor() {}
+
+    ngOnInit() {
+        if (this.dependencies) {
+            let length: number = this.dependencies.length;
+            let dependency: any, eachOne: any;
+            for (let i: number = 0; i < length; ++ i) {
+                dependency = {};
+                eachOne = this.dependencies[i];
+                dependency['name'] = eachOne['name'];
+                dependency['curVersion'] = eachOne['curVersion'];
+                dependency['latestVersion'] = eachOne['latestVersion'];
+                dependency['dateAdded'] = eachOne['dateAdded'];
+                dependency['pubPopularity'] = eachOne['pubPopularity'];
+                dependency['enterpriseUsage'] = eachOne['enterpriseUsage'];
+                dependency['teamUsage'] = eachOne['teamUsage'];
+
+                this.dependenciesList.push(dependency);
+            }
+        }
+    }
 }
