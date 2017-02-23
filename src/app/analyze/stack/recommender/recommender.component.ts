6a02b95c4bcacbfa554f5bbf9abc115255d8c6e6
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-02-23 15:18:34 +0530

    Implemented Restore Recommendation Feature
    Changes in this commit:
    
    1. Handles the case for restoring already dismissed work item
    2. Disables create work item link when the work item is disabled
    3. Enables Restore link only when the item is dismissed
    
    Tracks #16.

--- src/app/analyze/stack/recommender/recommender.component.ts
+++ src/app/analyze/stack/recommender/recommender.component.ts
@@ -38,15 +38,73 @@ export class RecommenderComponent implements OnChanges {
     }
 
     /*
+     * canCreateWorkItem - takes a recommendation and returns a boolean
+     * Checks if it is ok to enable creation of a work item 
+     */
+    private canCreateWorkItem(recommendation: any): boolean {
+        return !recommendation.isDismissed;
+    }
+
+    /*
      *  handleDropDownClick - takes item, recommender, event and returns nothing
      *  Checks for which of the list item is clicked and transfers the call to 
      *  the specific function accordingly. 
      */
     private handleDropDownClick(item: any, recommender: any, event: Event): void {
-        if (item && item.identifier === 'CREATE_WORK_ITEM') {
-            this.handleCreateWorkItemAction([recommender]);
+        if (item) {
+            let identifier: string = item.identifier;
+            if (identifier === 'CREATE_WORK_ITEM') {
+                if (this.canCreateWorkItem(recommender)) {
+                    this.handleCreateWorkItemAction([recommender]);
+                } else {
+                    console.log ('Cannot create work item as it has been dismissed');
+                }
+            } else if (identifier === 'DISMISS') {
+                if (!recommender.isDismissed) {
+                    this.handleDismissWorkItemAction([recommender]);
+                } else {
+                    console.log ('Work item is already dismissed');
+                }
+            } else if (identifier === 'RESTORE') {
+                if (recommender.isDismissed) {
+                    this.handleRestoreWorkItemAction([recommender]);
+                } else {
+                    console.log ('Work item is not dismissed');
+                }
+            }
         }
         event.preventDefault();
+    }
+
+
+    /*
+     *  toggleWorkItem - takes recommendations array, a boolean and returns nothing
+     *  When true is passed to todo - It is Dismiss Work Item Flow,
+     *  When false is passed to todo - It is Restore work item flow     
+     */
+    private toggleWorkItem(recommendations: Array<any>, todo: boolean): void {
+        let length: number = recommendations.length;
+        for (let i: number = 0; i < length; ++ i) {
+            if (recommendations && recommendations.length > 0) {
+                recommendations[i].isDismissed = todo;
+            }
+        }
+    }
+
+    /*
+     *  handleDismissWorkItemAction - takes recommendations as Array and returns nothing
+     *  Dismisses selected work items
+     */
+    private handleDismissWorkItemAction(recommendations: Array<any>): void {
+        this.toggleWorkItem(recommendations, true);
+    }
+
+    /*
+     *  handleRestoreWorkItemAction - takes recommendations as Array and returns nothing
+     *  Restores the selected dismissed work item
+     */
+    private handleRestoreWorkItemAction(recommendations: Array<any>): void {
+        this.toggleWorkItem(recommendations, false);
     }
 
     /*
@@ -115,13 +173,19 @@ export class RecommenderComponent implements OnChanges {
         let length: number = recommendations.length;
         if (recommendations && length > 0) {
             for (let i: number = 0; i < length; ++ i) {
-                let item: any = {
-                    title: recommendations[i]['action'],
-                    description: recommendations[i]['message']
-                };
-                workItems.push(item);
+                if (this.canCreateWorkItem(recommendations[i])) {
+                    let item: any = {
+                        title: recommendations[i]['action'],
+                        description: recommendations[i]['message']
+                    };
+                    workItems.push(item);
+                }
             }
-            this.addWorkItems(workItems);
+            if (workItems.length > 0) {
+                this.addWorkItems(workItems);
+            } else {
+                console.log('Work items are empty and cannot be added');
+            }
         }
     }
 
@@ -199,6 +263,26 @@ export class RecommenderComponent implements OnChanges {
             recommendation.isChecked = this.isSelectAll;
             this.handleCheckBoxChange(recommendation, true);
         });
+    }
+
+
+    private getCurrentClass(item: any, recommendation: any): string {
+        let className: string = 'deactivate';
+        let identifier: string = item.identifier;
+
+        if (identifier === 'RESTORE') {
+            className = recommendation.isDismissed ? 'activate' : className;
+        } else {
+            className = !recommendation.isDismissed ? 'activate' : className;
+        }
+        return className;
+    }
+
+    private canCreateAllWorkItems(): boolean {
+        if (this.newRecommendations.length > 0) {
+            return this.newRecommendations.some(recommendation => recommendation.isDismissed === false || recommendation.isDismissed === undefined);
+        }
+        return false;
     }
 
 }
