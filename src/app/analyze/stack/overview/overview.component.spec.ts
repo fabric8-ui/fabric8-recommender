8358fb6d263f03263abf6ba5b232cd4642252a4a
jaivardhan <mailjai.vardhan@gmail.com>
2017-03-01 18:31:26 +0530

    added test cases for Overview component

--- src/app/analyze/stack/overview/overview.component.spec.ts
+++ src/app/analyze/stack/overview/overview.component.spec.ts
@@ -11,8 +11,20 @@ import { ChartComponent } from './chart-component';
 describe('StackOverviewComponent', () => {
     let component: OverviewComponent;
     let fixture: ComponentFixture<OverviewComponent>;
-
+    let stackOverviewData: any;
     beforeEach(async(() => {
+        stackOverviewData = {
+            dependencyChart: [
+                ['internal', 11],
+                ['external', 21]
+            ],
+            compUsageChart: [
+                ['in teams', 2],
+                ['in organizations', 3]
+            ],
+            CVEdata: ['CVE-2014-0001', 'CVE-2014-12345', 'CVE-2013-78934']
+        };
+
         TestBed.configureTestingModule({
             imports: [
                 CommonModule,
@@ -30,4 +42,35 @@ describe('StackOverviewComponent', () => {
         fixture.detectChanges();
         expect(component).toBeTruthy();
     });
+
+    it('setDependencyChartData have to be called', () => {
+        let overviewComponent: any = new OverviewComponent();
+        spyOn(overviewComponent, 'setDependencyChartData');
+        overviewComponent.initOverviewComponents(stackOverviewData);
+        expect(overviewComponent.setDependencyChartData).toHaveBeenCalled();
+    });
+
+    it('setCompUsageChartData have to be called', () => {
+        let overviewComponent: any = new OverviewComponent();
+        spyOn(overviewComponent, 'setCompUsageChartData');
+        overviewComponent.initOverviewComponents(stackOverviewData);
+        expect(overviewComponent.setCompUsageChartData).toHaveBeenCalled();
+    });
+
+    it('check if dependency data has been updated', () => {
+        let overviewComponent: any = new OverviewComponent();
+        overviewComponent.initOverviewComponents(stackOverviewData);
+        expect(overviewComponent.dependenciesChart.data.type).toEqual('donut');
+        expect(overviewComponent.dependenciesChart.chartOptions.donut.title)
+            .toEqual('32 Dependencies');
+    });
+
+    it('check if component usage data has been updated', () => {
+        let overviewComponent: any = new OverviewComponent();
+        overviewComponent.initOverviewComponents(stackOverviewData);
+        expect(overviewComponent.compUsageChart.data.type).toEqual('donut');
+        expect(overviewComponent.compUsageChart.chartOptions.donut.title)
+            .toEqual('Component usage');
+    });
+
 });
