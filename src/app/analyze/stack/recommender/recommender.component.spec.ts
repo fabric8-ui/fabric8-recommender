885542c4ba2057e0a611c35c33ac91a886375e0a
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-03-02 16:42:23 +0530

    Unit test cases for RecommenderComponent
    Changes in this commit
    
    1. Tests for changes in the HTML DOM onrendering of the input value.
    2. Ensures the function gets called on the respective button click.
    3. Checks if the className is properly received on various input levels
    	a. Should receive 'activate' on a few cases
    	b. Should receive 'deactivate' on a few cases.
    
    Tracks #41.

--- src/app/analyze/stack/recommender/recommender.component.spec.ts
+++ src/app/analyze/stack/recommender/recommender.component.spec.ts
@@ -16,6 +16,7 @@ import { AddWorkFlowService } from '../stack-details/add-work-flow.service';
 describe('StackRecommenderComponent', () => {
     let component: RecommenderComponent;
     let fixture: ComponentFixture<RecommenderComponent>;
+    let debugElement: DebugElement;
 
     let recommendation: any = {
       suggestion: 'Recommended',
@@ -71,11 +72,16 @@ describe('StackRecommenderComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    /* it('Should have recommendations as much as the input recommendations', async(() => {
-        this.fixture = TestBed.createComponent(RecommenderComponent);
-        const element: Element = this.fixture.nativeElement;
-        const componentIns: any = this.fixture.componentInstance;
-        componentIns.recommendations = [{
+    beforeEach(() => {
+      fixture = TestBed.createComponent(RecommenderComponent);
+      component = fixture.componentInstance;
+      debugElement = fixture.debugElement;
+      component.ngOnChanges();
+      fixture.detectChanges();
+    });
+
+    it('Should have recommendations as much as the input recommendations', () => {
+        component.recommendations = [{
           suggestion: 'Recommended',
           action: 'Add',
           message: 'vertex-missing',
@@ -124,16 +130,151 @@ describe('StackRecommenderComponent', () => {
             }
           ]
         }];
-        this.fixture.detectChanges();
-        componentIns.ngOnChanges();
-        console.log(element);
-        console.log(element.querySelectorAll('recommendation-group-item'));
-        expect(element.querySelectorAll('recommendation-group-item').length).toBe(this.fixture.componentInstance.recommendations.length + 1);
-    })); */
+        component.ngOnChanges();
+        fixture.detectChanges();
+
+        expect(debugElement.queryAll(By.css('.recommendation-list')).length).toBe(fixture.componentInstance.recommendations.length);
+    });
+
+    /**
+     * TODO: Handle No Recommendations case
+     */
+
+
+    it('Should have triggered the button click', async(() => {
+      let recComponent: any = fixture.componentInstance;
+      spyOn(recComponent, 'handleRecommendationAction');
+      let toggleButton = debugElement.nativeElement.querySelector('.dropdown-toggle');
+      toggleButton.click();
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(recComponent.handleRecommendationAction).toHaveBeenCalled();
+      });
+    }));
+
+    fit('For Create flow, Should return className \'activate\' and to be of type string', () => {
+      let comp: any = new RecommenderComponent(null);
+      let item: any = {
+          itemName: 'Create WorkItem',
+          identifier: 'CREATE_WORK_ITEM'
+      };
+      let rec: any = {
+        suggestion: 'Recommended',
+        action: 'Add',
+        message: 'vertex-missing',
+        isDismissed: false,
+        pop: [
+          {
+            itemName: 'Create WorkItem',
+            identifier: 'CREATE_WORK_ITEM'
+          }, {
+            itemName: 'Dismiss Recommendation',
+            identifier: 'DISMISS'
+          }, {
+            itemName: 'Restore Recommendation',
+            identifier: 'RESTORE'
+          }
+        ]
+      };
+
+      let className = comp.getCurrentClass(item, rec);
+      expect(typeof className === 'string').toBe(true);
+      expect(className).toBe('activate');
+    });
+
+    fit('For Dismiss flow, Should return className \'deactivate\' and of type string', () => {
+      let comp: any = new RecommenderComponent(null);
+      let item: any = {
+          itemName: 'Dismiss Recommendation',
+          identifier: 'DISMISS'
+      };
+      let rec: any = {
+        suggestion: 'Recommended',
+        action: 'Add',
+        message: 'vertex-missing',
+        isDismissed: true,
+        pop: [
+          {
+            itemName: 'Create WorkItem',
+            identifier: 'CREATE_WORK_ITEM'
+          }, {
+            itemName: 'Dismiss Recommendation',
+            identifier: 'DISMISS'
+          }, {
+            itemName: 'Restore Recommendation',
+            identifier: 'RESTORE'
+          }
+        ]
+      };
+
+      let className = comp.getCurrentClass(item, rec);
+      expect(typeof className === 'string').toBe(true);
+      expect(className).toBe('deactivate');
+    });
+
+    fit('For Restore case, Should return className \'activate\' and of type string', () => {
+      let comp: any = new RecommenderComponent(null);
+      let item: any = {
+        itemName: 'Restore Recommendation',
+        identifier: 'RESTORE'
+      };
+      let rec: any = {
+        suggestion: 'Recommended',
+        action: 'Add',
+        message: 'vertex-missing',
+        isDismissed: true,
+        pop: [
+          {
+            itemName: 'Create WorkItem',
+            identifier: 'CREATE_WORK_ITEM'
+          }, {
+            itemName: 'Dismiss Recommendation',
+            identifier: 'DISMISS'
+          }, {
+            itemName: 'Restore Recommendation',
+            identifier: 'RESTORE'
+          }
+        ]
+      };
+
+      let className = comp.getCurrentClass(item, rec);
+      expect(typeof className === 'string').toBe(true);
+      expect(className).toBe('activate');
+    });
+
+    fit('For Restore case, Should return className \'deactivate\' and of type string', () => {
+      let comp: any = new RecommenderComponent(null);
+      let item: any = {
+        itemName: 'Restore Recommendation',
+        identifier: 'RESTORE'
+      };
+      let rec: any = {
+        suggestion: 'Recommended',
+        action: 'Add',
+        message: 'vertex-missing',
+        isDismissed: false,
+        pop: [
+          {
+            itemName: 'Create WorkItem',
+            identifier: 'CREATE_WORK_ITEM'
+          }, {
+            itemName: 'Dismiss Recommendation',
+            identifier: 'DISMISS'
+          }, {
+            itemName: 'Restore Recommendation',
+            identifier: 'RESTORE'
+          }
+        ]
+      };
+
+      let className = comp.getCurrentClass(item, rec);
+      expect(typeof className === 'string').toBe(true);
+      expect(className).toBe('deactivate');
+    });
 
     it('Should return true for creating work items', () => {
         const recommenderComponent: any = new RecommenderComponent(null);
-        let recommendation: any = {
+        let rec: any = {
           suggestion: 'Recommended',
           action: 'Add',
           message: 'vertex-missing',
@@ -150,7 +291,7 @@ describe('StackRecommenderComponent', () => {
             }
           ]
         };
-        let flag: boolean = recommenderComponent.canCreateWorkItem(recommendation);
+        let flag: boolean = recommenderComponent.canCreateWorkItem(rec);
         expect(flag).toBe(true);
     });
 
