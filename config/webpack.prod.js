0a8a48e109fdd1b27932f5b765dc12a4921e067d
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-03-10 22:09:58 +0530

    Changes in the text.
    1. Change of library name from Planner to fabric8-stack-analysis
    2. Change of description in package.json

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -46,8 +46,8 @@ module.exports = webpackMerge(commonConfig, {
   output: {
     path: helpers.root('dist'),
     publicPath: METADATA.PUBLIC_PATH,
-    filename: 'bundles/fabric8-planner.js',
-    library: 'fabric8-planner',
+    filename: 'bundles/fabric8-stack-analysis.js',
+    library: 'fabric8-stack-analysis',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
