0dcc6c617083cbecce060945d5849733d6d82a56
joshuawilson <javajoshw@gmail.com>
2017-03-28 22:02:43 -0400

    fix(build): add ngx-base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { Broadcaster } from 'ngx-base';
+import { Contexts } from 'ngx-fabric8-wit';
+import { AuthenticationService, AUTH_API_URL } from 'ngx-login-client';
+
 import { AppComponent }  from './app.component';
 
 import { witApiUrlProvider } from './shared/wit-api.provider';
 import { ApiLocatorService } from './shared/api-locator.service';
 import { authApiUrlProvider } from './shared/auth-api.provider';
-import { AuthenticationService, Broadcaster, AUTH_API_URL } from 'ngx-login-client';
-import { Contexts } from 'ngx-fabric8-wit';
 
 // Imports stackdetailsmodule
 import { StackDetailsModule } from './stack/stack-details/stack-details.module';
