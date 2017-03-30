1d8cd0aa142ce957adad79d28638fe116f0c3ae9
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-03-30 13:12:44 +0530

    Fixes #56. Includes provider for sso.api.url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { Broadcaster } from 'ngx-base';
 import { Contexts } from 'ngx-fabric8-wit';
-import { AuthenticationService, AUTH_API_URL } from 'ngx-login-client';
+import { AuthenticationService, AUTH_API_URL, SSO_API_URL } from 'ngx-login-client';
 
 import { AppComponent }  from './app.component';
 
 import { witApiUrlProvider } from './shared/wit-api.provider';
 import { ApiLocatorService } from './shared/api-locator.service';
 import { authApiUrlProvider } from './shared/auth-api.provider';
+import { ssoApiUrlProvider } from './shared/sso-api.provider';
 
 // Imports stackdetailsmodule
 import { StackDetailsModule } from './stack/stack-details/stack-details.module';
@@ -23,6 +24,7 @@ import { StackDetailsModule } from './stack/stack-details/stack-details.module';
     ApiLocatorService,
     witApiUrlProvider,
     authApiUrlProvider,
+    ssoApiUrlProvider,
     Contexts
   ],
   bootstrap:    [ AppComponent ]
