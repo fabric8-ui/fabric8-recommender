891700efcfae6515d6de86b096c9bd50dd89fe99
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-04-06 15:30:49 +0530

    fix(url): change the url from hardcode to handle dynamic scenario

--- src/app/stack/stack-analyses.service.ts
+++ src/app/stack/stack-analyses.service.ts
@@ -1,19 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
+import { WIT_API_URL } from 'ngx-fabric8-wit';
+
 @Injectable()
 export class StackAnalysesService {
 
-  private stackAnalysesUrl;
+  private stackAnalysesUrl: string = '';
+
+  private recommenderString: string = 'recommender';
+  private doubleSlash: string = '//';
+  private dot: string = '.';
+  private singleSlash: string = '/';
+  private v1Extension: string = 'v1';
 
   constructor(
     private http: Http,
+    @Inject(WIT_API_URL) apiUrl: string,
   ) {
-    this.stackAnalysesUrl = 'http://recommender.api.prod-preview.openshift.io/api/v1/';
+    if(process && process.env && process.env.FABRIC8_RECOMMENDER_API) {
+      this.stackAnalysesUrl = process.env.FABRIC8_RECOMMENDER_API;
+    } else {
+      if(apiUrl) {
+        this.stackAnalysesUrl = apiUrl.replace(this.doubleSlash, this.doubleSlash + this.recommenderString + this.dot);
+        let len: number = this.stackAnalysesUrl.length;
+        if(this.stackAnalysesUrl[len - 1] !== this.singleSlash) {
+          this.stackAnalysesUrl += this.singleSlash;
+        }
+        this.stackAnalysesUrl += this.v1Extension + this.singleSlash;
+      }
+    }
   }
 
   getStackAnalyses(id: string): Observable<any> {
