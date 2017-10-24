d54a13fac291272d3b8c1afa0127a0ec7e2ba84f
Jyasveer Gotta <jyasveer@gmail.com>
2017-10-24 16:03:46 +0530

    fix to handle if license data is empty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,16 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 
 export class AppComponent implements OnInit {
   public stackUrl: string =
-    'https://recommender.api.openshift.io/api/v1/stack-analyses/421249d9e1e5464cbf3e77dde4941463';
+    'https://recommender.api.openshift.io/api/v1/stack-analyses/aafb40f724804b919e0f2eca62a02c81';
 
   constructor() {}
   ngOnInit(): void {
     console.log('Inside ngInit');
   }
+
+  // Request Ids for diff starter apps to test
+  // vertex booster = aafb40f724804b919e0f2eca62a02c81
+  // springboot health check = 7b97b7c12d774a1db54ece52f861621c
+  // spring boot http = 7dfca1da25d7489796059754af8681ad
+  // spring boot crud = 826f7f7d0a184ea08b6838d59c557bca
 }
