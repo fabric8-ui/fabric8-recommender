30dc2d3c1f50380804831f21325cf2a86fd37340
joshuawilson <javajoshw@gmail.com>
2017-10-31 15:25:47 -0400

    fix(package): update RxJS 5.5. API reference calls

--- src/app/stack/stack-analyses.service.ts
+++ src/app/stack/stack-analyses.service.ts
@@ -3,7 +3,7 @@ import { Http, Response, Headers, RequestOptions  } from '@angular/http';
 import { AuthenticationService } from 'ngx-login-client';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
+import 'rxjs/operators/map';
 
 import { WIT_API_URL } from 'ngx-fabric8-wit';
 
