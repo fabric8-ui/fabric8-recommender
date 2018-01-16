f4fb76ae874e0cd7b3b7a890330ae01eb91cb86d
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2018-01-16 21:20:25 +0530

    test cases for report summary description

--- src/app/stack/report-summary/report-summary-card/report-summary-title/report-summary-title.component.spec.ts
+++ src/app/stack/report-summary/report-summary-card/report-summary-title/report-summary-title.component.spec.ts
@@ -1,12 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Http, ConnectionBackend, RequestOptions, BaseRequestOptions } from '@angular/http';
 
-
+import { MReportSummaryTitle } from '../../../models/ui.model';
 import { ReportSummaryTitleComponent } from './report-summary-title.component';
 
 describe ('ReportSummaryTitleComponent', () => {
     let component: ReportSummaryTitleComponent;
     let fixture: ComponentFixture<ReportSummaryTitleComponent>;
+
+    let title = new MReportSummaryTitle();
+    title.notificationIcon = 'icon-notification';
+    title.notificationIconBgColor = '#000000';
+    title.titleIcon = 'icon-title';
+    title.titleText = 'Title Text';
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -24,10 +30,15 @@ describe ('ReportSummaryTitleComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(ReportSummaryTitleComponent);
         component = fixture.componentInstance;
+        component.title = title;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('check if the input received is of the type MReportSummaryTitle', () => {
+        expect(component.title instanceof MReportSummaryTitle).toBeTruthy();
     });
 });
