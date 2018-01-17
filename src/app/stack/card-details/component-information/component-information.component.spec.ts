23daf1f1aedb4dbb9e7888d7eacbb01778f7696f
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2018-01-18 00:08:18 +0530

    unit tests for compnent information component

--- src/app/stack/card-details/component-information/component-information.component.spec.ts
+++ src/app/stack/card-details/component-information/component-information.component.spec.ts
@@ -22,9 +22,60 @@ import { ssoApiUrlProvider } from '../../../shared/sso-api.provider';
 import { realmProvider } from '../../../shared/realm-token.provider';
 import { MockAuthenticationService } from '../../../shared/mock-auth.service';
 
+import {
+    MRecommendationInformation,
+    MComponentInformation,
+    MGenericStackInformation,
+    MComponentHeaderColumn,
+    MGithub,
+    MOsio
+} from '../../models/ui.model';
+
 describe ('ComponentInformationComponent', () => {
     let component: ComponentInformationComponent;
     let fixture: ComponentFixture<ComponentInformationComponent>;
+    let element: HTMLElement;
+    let genericInformation: MGenericStackInformation = new MGenericStackInformation(
+        '',
+        ''
+    );
+    let positions: Array<MComponentHeaderColumn> = [new MComponentHeaderColumn(
+        'component',
+        'component name',
+        'class-name',
+        false,
+        false
+    )];
+    let componentInformation: MComponentInformation = new MComponentInformation (
+        'component',
+        '1.3.2',
+        '1.4.3',
+        null,
+        false,
+        false,
+        false,
+        null,
+        null,
+        new MGithub(
+            2,
+            3,
+            4,
+            5,
+            3,
+            null
+        ),
+        new MOsio(
+            3
+        ),
+        '',
+        false,
+        null,
+        false,
+        null,
+        '',
+        '',
+        null
+    );
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -60,10 +111,79 @@ describe ('ComponentInformationComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(ComponentInformationComponent);
         component = fixture.componentInstance;
+        element = fixture.nativeElement;
+        component.component = componentInformation;
+        component.genericInformation = genericInformation;
+        component.positions = positions;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('check if the input received in "component" is either of type  MComponentInformation or MRecommendationInformation', () => {
+        expect(component.component instanceof MComponentInformation || component.component instanceof MRecommendationInformation).toBeTruthy();
+    });
+
+    it('check if the input received in "genericInformation" is of type  MGenericStackInformation', () => {
+        expect(component.genericInformation instanceof MGenericStackInformation).toBeTruthy();
+    });
+
+    it('check if the input received in "positions" is of type MComponentHeaderColumn', () => {
+        expect(component.positions[0] instanceof MComponentHeaderColumn).toBeTruthy();
+    });
+
+    it('should expand if "isOpen" is true', () => {
+        (<MComponentInformation>component.component).isOpen = true;
+        fixture.detectChanges();
+        let parent = element.querySelector('.analytics-component-information');
+        expect(parent.querySelector('.accordion-body')).toBeDefined();
+    });
+
+    it('should be normal if "isOpen" is false', () => {
+        (<MComponentInformation>component.component).isOpen = false;
+        fixture.detectChanges();
+        let parent = element.querySelector('.analytics-component-information');
+        expect(parent.querySelector('.accordion-body')).toBeNull();
+    });
+
+    it('should have a split view if recommendation is present with component', () => {
+        (<MComponentInformation>component.component).isOpen = true;
+        (<MComponentInformation>component.component).recommendation = new MRecommendationInformation(
+            'alternate',
+            'This is the reason',
+            null,
+            null,
+            new MComponentInformation(
+                'alternate-1',
+                '',
+                '',
+                null,
+                false,
+                false,
+                false,
+                null,
+                null,
+                null,
+                null,
+                'Create work item',
+                false,
+                null,
+                false,
+                null,
+                '',
+                '',
+                null
+            ),
+            '',
+            null
+        );
+        fixture.detectChanges();
+        let parent = element.querySelector('.analytics-component-information');
+        expect(parent.querySelector('.accordion-body').children.length).toBe(2);
+    });
+
+    // TODO should improve coverage
+
 });
